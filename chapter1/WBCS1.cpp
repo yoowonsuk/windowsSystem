8071da8e094f818a05fb5b5f79fe0001e686d815
yoowonsuk <52575749+yoowonsuk@users.noreply.github.com>
2021-03-29 17:01:52 +0900

    Create WBCS1.cpp

---
+++ chapter1/WBCS1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    wchar_t str[] = L"ABC";
+    int size = sizeof(str);
+    int len = wcslen(str);
+    
+    printf("배열의 크기: %d \n", size);
+    printf("문자열 길이: %d \n", len);
+
+    return 0;
+}
