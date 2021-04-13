afba9fa92008e93c113ff269703deff1763452e8
yoowonsuk <52575749+yoowonsuk@users.noreply.github.com>
2021-04-13 09:11:12 +0900

    Update AdderProcess.cpp

--- ch5CreateProcess/AdderProcess.cpp
+++ ch5CreateProcess/AdderProcess.cpp
@@ -1,6 +1,5 @@
 /*
 	AdderProcess.cpp
-	프로그램 설명: main 함수의 전달인자를 덧셈하는 프로그램
 */
 
 #include <stdio.h>
@@ -13,8 +12,8 @@ int _tmain(int argc, TCHAR* argv[])
 	val1 = _ttoi(argv[1]);
 	val2 = _ttoi(argv[2]);
 
-	_tprintf(_T("%d + %d = %d \n"), val1, val2, val1+val2);
+	_tprintf(_T("%d + %d = %d \n"), val1, val2, val1 + val2);
 
-	_gettchar(); // 프로그램의 실행을 잠시 멈추기 위해
+	_gettchar(); // pause
 	return 0;
 }
