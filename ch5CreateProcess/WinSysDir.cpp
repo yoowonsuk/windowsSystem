2e4ffc598d1d7804f70b0532b9d0bcd8db6d5282
yoowonsuk <52575749+yoowonsuk@users.noreply.github.com>
2021-04-13 09:17:02 +0900

    Create WinSysDir.cpp

---
+++ ch5CreateProcess/WinSysDir.cpp
@@ -0,0 +1,23 @@
+/*
+	WinSysDir.cpp
+*/
+
+#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+
+#define DIR_LEN MAX_PATH+1
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	TCHAR sysDir[DIR_LEN];
+	TCHAR winDir[DIR_LEN];
+
+	GetSystemDirectory(sysDir, DIR_LEN);
+
+	GetWindowsDirectory(winDir, DIR_LEN);
+
+	_tprintf(_T("System Dir: %s \n"), sysDir);
+	_tprintf(_T("Window Dir: %s \n"), winDir);
+	return 0;
+}
