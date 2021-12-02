cd0e740bce805349eddd99a5cb507e0ca1f23f2e
yoowonsuk <52575749+yoowonsuk@users.noreply.github.com>
2021-12-02 16:55:54 +0900

    Create anonymous_pipe.cpp

---
+++ chapter8/anonymous_pipe.cpp
@@ -0,0 +1,36 @@
+/* 
+	anonymous_pipe.cpp
+*/
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+int _tmain(int argc, LPTSTR argv[])
+{
+	HANDLE hReadPipe, hWritePipe; //pipe handle
+
+	TCHAR sendString[] = _T("anonymous pipe");
+	TCHAR recvString[100];
+
+	DWORD bytesWritten;
+	DWORD bytesRead;          
+
+	/* pipe 생성 */
+	CreatePipe(&hReadPipe, &hWritePipe, NULL, 0);
+
+	/* pipe의 한쪽 끝을 이용한 데이터 송신 */ 
+	WriteFile(hWritePipe, sendString, lstrlen(sendString)*sizeof(TCHAR), &bytesWritten, NULL);
+	_tprintf( _T("string send: %s \n"), sendString);
+
+
+	/* pipe의 다른 한쪽 끝을 이용한 데이터 수신 */ 
+	ReadFile(hReadPipe, recvString, bytesWritten, &bytesRead, NULL);
+	recvString[bytesRead/sizeof(TCHAR)] = 0;
+	_tprintf( _T("string recv: %s \n"), recvString);
+
+
+	CloseHandle(hReadPipe);
+	CloseHandle(hWritePipe);
+
+	return 0;
+}
