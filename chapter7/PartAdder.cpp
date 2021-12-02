aa893d5aab7cf8a5cff327abfdff8b2d2c2576bd
yoowonsuk <52575749+yoowonsuk@users.noreply.github.com>
2021-12-02 15:46:52 +0900

    Create PartAdder.cpp

---
+++ chapter7/PartAdder.cpp
@@ -0,0 +1,23 @@
+/*
+	PartAdder.cpp
+*/
+
+#include <stdio.h>
+#include <windows.h>
+#include <tchar.h>
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	if (argc != 3)
+		return -1;
+
+	DWORD start = _ttoi(argv[1]);
+	DWORD end = _ttoi(argv[2]);
+
+	DWORD total = 0;
+
+	for (DWORD i = start; i <= end; i++)
+		total += i;
+
+	return total;
+}
