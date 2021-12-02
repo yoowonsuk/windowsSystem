76ba22a74af481f36fff9f6ad0a7b549319a6778
yoowonsuk <52575749+yoowonsuk@users.noreply.github.com>
2021-12-02 21:38:29 +0900

    Update CommandPrmpt.cpp

--- commandPrmpt/CommandPrmpt.cpp
+++ commandPrmpt/CommandPrmpt.cpp
@@ -1,76 +1,70 @@
 /*
-	CommandPrmpt.cpp
-	프로그램 설명: 명령 프롬프트의 골격
+	CommandPrmpt_Four.cpp
+	프로그램 설명: 명령 프롬프트 4차
+	  명령어 1: lp --> List process. (제공)
+	  멸영어 2: kp --> Kill process. (구현)
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <tchar.h>
 #include <locale.h>
-#include <Windows.h>
+#include <windows.h> 
+#include <tlhelp32.h>
 
-#define STR_LEN 256
-#define CMD_TOKEN_NUM 10
+#define STR_LEN    256
+#define CMD_TOKEN_NUM  10
 
-TCHAR ERROR_CMD[] = _T("'%s'은(는) 실행할 수 있는 프로그램이 아닙니다. \n");
-
-int CmdProcessing(int);
-TCHAR * StrLower(TCHAR *);
-int CmdReadTokenize(void);
-void CmdInput(void);
-
+TCHAR ERROR_CMD[]
+= _T("'%s'은(는) 실행할 수 있는 프로그램이 아닙니다. \n");
 
 TCHAR cmdString[STR_LEN];
 TCHAR cmdTokenList[CMD_TOKEN_NUM][STR_LEN];
 TCHAR seps[] = _T(" ,\t\n");
 
-int _tmain(int argc, TCHAR * argv[])
+int CmdReadTokenize(void);
+int CmdProcessing(int);
+TCHAR* StrLower(TCHAR*);
+
+int _tmain(int argc, TCHAR* argv[])
 {
-	// 한글 입력을 가능케 하기 위해
+	// 한글 입력을 가능케 하기 위해.
 	_tsetlocale(LC_ALL, _T("Korean"));
 
-	/*
-	int i;
-	_tprintf("argc: %d\n", argc);
-	for (i = 0; i < argc; i++)
-		_tprintf("argv[%d]: %s\n", i, argv[i]);
-	*/
-
-	// start command
-	if (argc > 1)
+	if (argc > 2)  // 매개변수 전달인자가 있는 경우.
 	{
-		int i;
-		for (i = 1; i < argc; i++)
+		for (int i = 1; i < argc; i++)
 			_tcscpy(cmdTokenList[i - 1], argv[i]);
-		CmdProcessing(argc - 1); // start 이후 (미포함) 명령어 처리
+
+		CmdProcessing(argc - 1);
 	}
 
-	DWORD isExit;
+	DWORD isExit = NULL;
 	while (1)
 	{
-		CmdInput();
 		int tokenNum = CmdReadTokenize();
+		if (tokenNum == 0) //Enter 입력시 처리를 위해...
+			continue;
+
 		isExit = CmdProcessing(tokenNum);
+		if (isExit == TRUE)
 		{
-			if (isExit == TRUE)
-			{
-				_fputts(_T("명령어 처리를 종료합니다. \n"), stdout);
-				break;
-			}
+			_fputts(_T("명령어 처리를 종료합니다. \n"), stdout);
+			break;
 		}
 	}
 
 	return 0;
 }
-/**********************************************************************
-함수: int CmdReadTokenize(void)
-기능: CommandPrmpt_Two.cpp의 CmdProcessing 함수는 사용자의 선택을 입력받는 기능과
-선택에 따른 명령어 처리 기능을 동시에 지니고 있다. 이에 사용자의 선택을 입력받는 기능을
-CmdreadTokenize 함수로 분리시켰다. 명령어가 main 함수를 통해 전달되는 경우에는 사용자 입력이 불필요하기 때문이다.
-**********************************************************************/
+
 int CmdReadTokenize(void)
 {
-	TCHAR * token = _tcstok(cmdString, seps);
+	TCHAR* token;
+
+	_fputts(_T("Best command prompt>> "), stdout);
+	_getts(cmdString);
+
+	token = _tcstok(cmdString, seps);
+
 	int tokenNum = 0;
 
 	while (token != NULL)
@@ -82,71 +76,147 @@ int CmdReadTokenize(void)
 	return tokenNum;
 }
 
-void CmdInput(void)
+void ListProcessInfo(void)
 {
-	_fputts(_T("Best command prompt>> "), stdout);
-	_getts_s(cmdString); // _getts not working
+	HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hProcessSnap == INVALID_HANDLE_VALUE)
+	{
+		_tprintf(_T("CreateToolhelp32Snapshot error! \n"));
+		return;
+	}
+
+	PROCESSENTRY32 pe32;	/* 프로세스 정보 얻기위한 구조체 변수 */
+
+	/* PROCESSENTRY32 변수는 사용하기 전에 크기 정보 초기화 해야한다 */
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+
+	/* 첫 번째 프로세스 정보 얻음 */
+	if (!Process32First(hProcessSnap, &pe32))
+	{
+		_tprintf(_T("Process32First error! \n"));
+		CloseHandle(hProcessSnap);
+		return;
+	}
+
+	do
+	{
+		/* 프로세스 이름, ID 정보 출력 */
+		_tprintf(_T("%25s %5d \n"), pe32.szExeFile, pe32.th32ProcessID);
+
+	} while (Process32Next(hProcessSnap, &pe32));
+
+	CloseHandle(hProcessSnap);
 }
-/**********************************************************************
-함수: THCAR int CmdProcessing(void)   // TCHAR 의미가..?
-기능: 명령어를 입력 받아서 해당 명령어에 지정되어 있는 기능을 수행한다.
-	  exit가 입력되면 TRUE를 반환하고 이는 프로그램의 종료로 이어진다.
-**********************************************************************/
+
+void KillProcess(void)
+{
+	HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hProcessSnap == INVALID_HANDLE_VALUE)
+	{
+		_tprintf(_T("CreateToolhelp32Snapshot (of processes)"));
+		return;
+	}
+
+	PROCESSENTRY32 pe32;
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+
+	if (!Process32First(hProcessSnap, &pe32))
+	{
+		_tprintf(_T("Process32First"));
+		CloseHandle(hProcessSnap);
+		return;
+	}
+
+	HANDLE hProcess;
+	BOOL isKill = FALSE;
+	do
+	{
+		if (_tcscmp(pe32.szExeFile, cmdTokenList[1]) == 0)
+		{
+			hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
+
+			if (hProcess != NULL)
+			{
+				TerminateProcess(hProcess, -1);
+				isKill = TRUE;
+			}
+
+			CloseHandle(hProcess);
+			break;
+		}
+
+	} while (Process32Next(hProcessSnap, &pe32));
+
+	CloseHandle(hProcessSnap);
+
+	if (isKill == FALSE)
+		_tprintf(_T("Kill process fail, Try again! \n"));
+}
+
 int CmdProcessing(int tokenNum)
 {
+	BOOL isRun;
+	STARTUPINFO si = { 0, };
+	PROCESS_INFORMATION pi;
 
+	TCHAR cmdStringWithOptions[STR_LEN] = { 0, };
+	TCHAR optString[STR_LEN] = { 0, };
+
+	si.cb = sizeof(si);
 	if (!_tcscmp(cmdTokenList[0], _T("exit")))
+	{
 		return TRUE;
-
+	}
 	else if (!_tcscmp(cmdTokenList[0], _T("start")))
 	{
-		TCHAR optString[STR_LEN] = { 0 };
-		TCHAR cmdStringWithOptions[STR_LEN] = { 0 };
 		if (tokenNum > 1)
 		{
-			int i;
-			for (i = 1; i < tokenNum; i++)
+			for (int i = 1; i < tokenNum; i++)
 				_stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
+
+			_stprintf(cmdStringWithOptions, _T("%s %s"), _T("CmdProject.exe"), optString);
 		}
-		_stprintf(cmdStringWithOptions, _T("%s %s"), _T("Project3.exe"), optString); // Project3.exe (start) (command)
-		STARTUPINFO si = { 0, };
-		PROCESS_INFORMATION pi;
-		si.cb = sizeof(si);
-		BOOL isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+		else
+			_stprintf(cmdStringWithOptions, _T("%s"), _T("CmdProject.exe"));
+
+		isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+
 		CloseHandle(pi.hProcess);
 		CloseHandle(pi.hThread);
 	}
-
 	else if (!_tcscmp(cmdTokenList[0], _T("echo")))
 	{
-		TCHAR optString[STR_LEN] = { 0 };
-		TCHAR cmdStringWithOptions[STR_LEN] = { 0 };
-		int i;
-		for (i = 1; i < tokenNum; i++)
+		for (int i = 1; i < tokenNum; i++)
 			_stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
-		_tprintf(_T("echo message: %s \n"), optString);
-	}
 
-	else if (!_tcscmp(cmdTokenList[0], _T("추가 되는 명령어")))
+		_tprintf(_T("echo message:%s \n"), optString);
+	}
+	else if (!_tcscmp(cmdTokenList[0], _T("lp")))
 	{
-
+		ListProcessInfo();
 	}
+	else if (!_tcscmp(cmdTokenList[0], _T("kp")))
+	{
+		if (tokenNum < 2)
+		{
+			_tprintf(_T("usage: kp <process name> \n"));
+			return 0;
+		}
 
+		KillProcess();
+	}
 	else
 	{
-		TCHAR optString[STR_LEN] = { 0 };
-		_tcscpy(optString, cmdTokenList[0]);
+		_tcscpy(cmdStringWithOptions, cmdTokenList[0]);
 
-		int i;
-		for (i = 1; i < tokenNum; i++)
-			_stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
+		for (int i = 1; i < tokenNum; i++)
+			_stprintf(cmdStringWithOptions, _T("%s %s"), cmdStringWithOptions, cmdTokenList[i]);
 
-		STARTUPINFO si = { 0, };
-		PROCESS_INFORMATION pi;
-		si.cb = sizeof(si);
-		BOOL isRun = CreateProcess(NULL, optString, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+		isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+
 		CloseHandle(pi.hProcess);
 		CloseHandle(pi.hThread);
+
 		if (isRun == FALSE)
 			_tprintf(ERROR_CMD, cmdTokenList[0]);
 	}
@@ -154,19 +224,16 @@ int CmdProcessing(int tokenNum)
 	return 0;
 }
 
-/**********************************************************************
-함수: TCHAR * StrLower (TCHAR *pStr)
-기능: 문자열의 내에 존재하는 모든 대문자를 소문자로 변경한다.
-	  변경된 문자열의 포인터를 반환한다.
-**********************************************************************/
-TCHAR * StrLower(TCHAR * pStr)
+
+TCHAR* StrLower(TCHAR* pStr)
 {
-	TCHAR * ret = pStr;
+	TCHAR* ret = pStr;
 
 	while (*pStr)
 	{
 		if (_istupper(*pStr))
 			*pStr = _totlower(*pStr);
+
 		pStr++;
 	}
 
